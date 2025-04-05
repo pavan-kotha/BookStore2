4a6a167f23d53bf03cbf33e09ab81d2516e3ec78
DummyKotha <pavankothakp@gmail.com>
2025-04-05 10:42:30 +0530

    Initial commit

---
+++ catalog-service/src/main/java/com/pavankotha/catalogservice/CatalogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pavankotha.catalogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogServiceApplication.class, args);
+	}
+
+}
